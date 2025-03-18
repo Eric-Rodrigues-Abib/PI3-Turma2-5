819f45ec9829a6a26aacea22441949c730f665dc
Eric Abib <ehheric@gmail.com>
2025-03-18 19:32:55 -0300

    userpreferences esta feito

--- app/src/main/java/com/example/pi3_turma2_5/MainActivity.kt
+++ app/src/main/java/com/example/pi3_turma2_5/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.example.pi3_turma2_5
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.pi3_turma2_5.userPreferences.PreferencesHelper
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.debug.DebugAppCheckProviderFactory
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val preferencesHelper = PreferencesHelper.getInstance(this)
+        //navController = findNavController(R.id.nav_host_fragment)
+
+        //if(preferencesHelper.infoBool && preferencesHelper.termsAndConditionBool){
+            //navController.navigate(R.id.action_global_loginfragment)
+        //}
+
     }
+
+    private fun prepareFirebaseAppCheckDebug(){
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            DebugAppCheckProviderFactory.getInstance()
+        )
+    }
+
 }
---
+++ app/src/main/java/com/example/pi3_turma2_5/userPreferences/PreferencesHelper.kt
@@ -0,0 +1,37 @@
+package com.example.pi3_turma2_5.userPreferences
+
+import android.content.Context
+import androidx.core.content.edit
+
+class PreferencesHelper private constructor(context: Context){
+    private val sharedPreferences =
+        context.applicationContext.getSharedPreferences(USER_PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+    var uid: String
+        get() = sharedPreferences.getString(UID_KEY, "") ?: ""
+        set(value) = sharedPreferences.edit() { putString(UID_KEY, value) }
+
+    var infoBool: Boolean
+        get() = sharedPreferences.getBoolean(INFO_BOOL, false)
+        set(value) = sharedPreferences.edit() { putBoolean(INFO_BOOL, value) }
+
+    var termsAndConditionBool: Boolean
+        get() = sharedPreferences.getBoolean(TERMSANDCONDITION_BOOL, false)
+        set(value) = sharedPreferences.edit() { putBoolean(TERMSANDCONDITION_BOOL, value) }
+
+    companion object {
+        private const val USER_PREFERENCES_NAME = "prefs_tokens"
+        private const val UID_KEY = "uid"
+        private const val INFO_BOOL = "info_bool"
+        private const val TERMSANDCONDITION_BOOL = "termsandcondition_bool"
+
+        @Volatile
+        private var instance: PreferencesHelper? = null
+
+        fun getInstance(context: Context): PreferencesHelper {
+            return instance ?: synchronized(this) {
+                instance ?: PreferencesHelper(context).also { instance = it }
+            }
+        }
+    }
+}
