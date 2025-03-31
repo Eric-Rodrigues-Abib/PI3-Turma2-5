2713a488e457e4009add2fe8aa8c63c55472b84d
Eric Abib <erabib@buckman.com>
2025-03-31 20:44:54 -0300

    Login funcionando e nav graph para a nova parte do projeto

---
+++ app/src/main/java/com/example/pi3_turma2_5/FeatureActivity.kt
@@ -0,0 +1,16 @@
+package com.example.pi3_turma2_5
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class FeatureActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feature)
+    }
+}
