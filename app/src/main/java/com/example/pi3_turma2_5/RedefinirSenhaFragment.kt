f6b4abbeeebaa27ebe200631903d88588c1062d3
Eric-Rodrigues-Abib <ehheric@gmail.com>
2025-05-20 17:20:38 -0300

    ajustes na tela de recovery

--- app/src/main/java/com/example/pi3_turma2_5/RedefinirSenhaFragment.kt
+++ app/src/main/java/com/example/pi3_turma2_5/RedefinirSenhaFragment.kt
@@ -9,6 +9,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.pi3_turma2_5.databinding.FragmentRedefinirSenhaBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class RedefinirSenhaFragment : Fragment() {
@@ -17,11 +19,19 @@ class RedefinirSenhaFragment : Fragment() {
     private lateinit var btnEnviar: Button
     private lateinit var auth: FirebaseAuth
 
+    private var _binding: FragmentRedefinirSenhaBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_redefinir_senha, container, false)
+        _binding = FragmentRedefinirSenhaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         etEmail = view.findViewById(R.id.etEmailRedefinir)
         btnEnviar = view.findViewById(R.id.btnEnviarNovaSenha)
@@ -36,15 +46,26 @@ class RedefinirSenhaFragment : Fragment() {
                 auth.sendPasswordResetEmail(email)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
-                            Toast.makeText(requireContext(), "Email de redefinição enviado", Toast.LENGTH_LONG).show()
+                            Toast.makeText(
+                                requireContext(),
+                                "Email de redefinição enviado",
+                                Toast.LENGTH_LONG
+                            ).show()
                             etEmail.setText("")
+                            findNavController().navigate(R.id.action_redefinirSenhaFragment_to_logInFragment)
                         } else {
-                            Toast.makeText(requireContext(), "Erro: ${task.exception?.message}", Toast.LENGTH_LONG).show()
+                            Toast.makeText(
+                                requireContext(),
+                                "Erro: ${task.exception?.message}",
+                                Toast.LENGTH_LONG
+                            ).show()
                         }
                     }
             }
         }
+    }
 
-        return view
+    override fun onDestroyView() {
+        super.onDestroyView()
     }
 }
